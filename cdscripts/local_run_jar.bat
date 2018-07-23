348e7bbd28cd986ab36b32ffd692cfcfa39df992
flomintv <flomintv@alfastrah.ru>
2018-07-23 08:59:28 +0300

    Windows run script has been fixed

--- cdscripts/local_run_jar.bat
+++ cdscripts/local_run_jar.bat
@@ -3,7 +3,7 @@ call mvn clean package
 for /f %%i in ('call mvn -q --non-recursive "-Dexec.executable=cmd" "-Dexec.args=/C echo ${project.version}" "org.codehaus.mojo:exec-maven-plugin:exec"') do set VERSION=%%i
 set ARTIFACT="accounts-money-transfer"
 
-# for debugging purposes
-#java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar ./target/$ARTIFACT-$VERSION-jar-with-dependencies.jar
+rem for debugging purposes
+rem java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar ./target/$ARTIFACT-$VERSION-jar-with-dependencies.jar
 
-java -jar ./target/$ARTIFACT-$VERSION-jar-with-dependencies.jar
+java -jar ./target/%ARTIFACT%-%VERSION%-jar-with-dependencies.jar
